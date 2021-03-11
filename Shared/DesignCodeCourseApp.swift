fb9423ac713c87d41f13a22fc1bf79fa9ac9f54a
charlettabullard <charletta.bullard@protonmail.com>
2021-03-11 15:43:13 -0500

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Charletta Bullard on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
